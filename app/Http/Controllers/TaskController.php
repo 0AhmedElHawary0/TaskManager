3209be6e20a4287bb9d4773b5a6fb21d471342e5
Hawary <Hawary@DESKTOP-VA1OU9Q>
2025-04-28 22:34:52 +0300

    New first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function store(Request $request)
+    {
+        Task::create([
+            'title'=>$request->title,
+            'description'=>$request->description,
+            'priority'=>$request->priority
+        ]);
+    }
+}
